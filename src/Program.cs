41eee77a663f82a5965b8084e8c020fa22d70a48
Dinap Servidor <angel@dinaup.com>
2025-02-18 07:53:28 +0100

    add errorfatalmiddleware

--- src/Program.cs
+++ src/Program.cs
@@ -2,49 +2,68 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Radzen;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddRadzenComponents();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<CurrentUserService>();
-
-
-
-
-
-// Dinaup -> Logs
-var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
-Dinaup.Logs.Initialize(nameof(ReadyToBlazor), version, new Dinaup.Logs.ElasticConfig());
-
-// Dinaup -> Client
-var configDinaup = builder.Configuration.GetSection("Dinaup").Get<DinaupSettings>();
-var configSMTP = builder.Configuration.GetSection("Smtp").Get<SmtpSettings>();
-
-var dinaupClient = new Dinaup.DinaupClientC(configDinaup.APIBaseUrl, configDinaup.APIKey, configDinaup.APISecret, "*");
-
-Task.Run(async ()=>{  await  dinaupClient.InitializeAsync(5000);});
-
-builder.Services.AddSingleton(dinaupClient);
-builder.Services.AddSingleton<SMTPClient>( new SMTPClient(configSMTP));
-builder.Services.AddSingleton<SMTPService>(  );
-
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+try
 {
-	app.UseExceptionHandler("/Error");
-	app.UseHsts();
+
+	var builder = WebApplication.CreateBuilder(args);
+
+	builder.Services.AddControllers();
+	builder.Services.AddRazorPages();
+	builder.Services.AddServerSideBlazor();
+	builder.Services.AddRadzenComponents();
+	builder.Services.AddHttpContextAccessor();
+	builder.Services.AddScoped<CurrentUserService>();
+
+
+
+
+	// Dinaup -> Logs
+	var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
+	Dinaup.Logs.Initialize(nameof(ReadyToBlazor), version, new Dinaup.Logs.ElasticConfig());
+
+	// Dinaup -> Client
+	var configDinaup = builder.Configuration.GetSection("Dinaup").Get<DinaupSettings>();
+	var configSMTP = builder.Configuration.GetSection("Smtp").Get<SmtpSettings>();
+
+	var dinaupClient = new Dinaup.DinaupClientC(configDinaup.APIBaseUrl, configDinaup.APIKey, configDinaup.APISecret, "*");
+
+	Task.Run(async () => { await dinaupClient.InitializeAsync(5000); });
+
+	builder.Services.AddSingleton(dinaupClient);
+	builder.Services.AddSingleton<SMTPClient>(new SMTPClient(configSMTP));
+	builder.Services.AddSingleton<SMTPService>();
+
+
+	var app = builder.Build();
+
+	if (!app.Environment.IsDevelopment())
+	{
+		app.UseExceptionHandler("/Error");
+		app.UseHsts();
+	}
+
+	app.UseMiddleware<MaintenanceMiddleware>();
+	app.UseHttpsRedirection();
+	app.UseStaticFiles();
+	app.UseRouting();
+	app.MapBlazorHub();
+	app.MapControllers();
+	app.MapFallbackToPage("/_Host");
+	app.Run();
+}
+catch (Exception)
+{
+	var builder2 = WebApplication.CreateBuilder(args);
+	var app2 = builder2.Build();
+	app2.UseMiddleware<ErrorFatalMiddleware>();
+	app2.Run();
+
+
+}
+finally
+{
+
+	Dinaup.Logs.CloseAndFlush();
+
 }
 
-app.UseMiddleware<MaintenanceMiddleware>();
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.MapBlazorHub();
-app.MapControllers();
-app.MapFallbackToPage("/_Host");
-app.Run();
---
+++ src/Services/ErrorFatalMiddleware.cs
@@ -0,0 +1,18 @@
+﻿ 
+
+public class ErrorFatalMiddleware
+{
+	private readonly RequestDelegate _next;
+
+	public ErrorFatalMiddleware(RequestDelegate next)
+	{
+		_next = next;
+	}
+
+	public async Task InvokeAsync(HttpContext context)
+	{
+		context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+		Dinaup.Logs.Fatal("Estamos experimentando inconvenientes al iniciar la aplicación. Por favor, inténtelo de nuevo en unos minutos. Gracias por su paciencia.");
+		await context.Response.WriteAsync("Estamos experimentando inconvenientes al iniciar la aplicación. Por favor, inténtelo de nuevo en unos minutos. Gracias por su paciencia.");
+	}
+}
